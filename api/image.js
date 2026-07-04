b9434863b09400ee9e05e52ad52d5e53af585909
mvillen647-crypto <mvillen647@gmail.com>
2026-07-04 17:18:34 +0300

    Update image.js

--- api/image.js
+++ api/image.js
@@ -2,6 +2,12 @@ import analyzeImage from "../sightengine.js";
 import formatResult from "../formatter.js";
 import { validateKey } from "../lib/keys.js";
 
+const setCors = (res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-api-key");
+};
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Only POST allowed" });
